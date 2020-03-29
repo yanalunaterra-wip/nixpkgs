016071dc319f9cd1d162fbfba3247eb277cc0272
Yegor Timoshenko <yegortimoshenko@riseup.net>
2020-03-29 01:48:01 +0100

    nixos/tests/h2o-etag: init

---
+++ nixos/tests/h2o-etag.nix
@@ -0,0 +1,18 @@
+import ./common/etag {
+  name = "h2o-etag";
+
+  serverConfig = { config, pkgs, ... }: let
+    h2oConfig = pkgs.writeText "h2o.conf" (builtins.toJSON {
+      listen.port = 80;
+      hosts.server.paths."/"."file.dir" = config.test-support.etag.root;
+    });
+  in {
+    systemd.services.h2o = {
+      after = [ "network-online.target" ];
+      wantedBy = [ "multi-user.target" ];
+      serviceConfig = {
+        ExecStart = "${pkgs.h2o}/bin/h2o --conf ${h2oConfig}";
+      };
+    };
+  };
+}
